5fe68c571b378a475d5176ceae2a32625f561fbf
DILAKSHI-ICT <96301@fhss.sjp.ac.lk>
2021-07-13 15:49:00 +0530

    Connected database to students details GUI

--- src/com/text/index/studentDetails.java
+++ src/com/text/index/studentDetails.java
@@ -4,8 +4,43 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public class studentDetails {
+
+    public void studentRegistration(int roll_num, String first_name, String name_initials, String address, String con_number, String gender, String email ){
+
+        try{
+            Connection connection = mysqlClass.getConnection();
+            String sqlQuery = "INSERT INTO `student` (`roll_no`, `first_name`, `name_with_initials`, `address`, `contact_number`, `gender`, `email`) VALUES (NULL, ?, ?, ?, ?, ?, ?)";
+
+            PreparedStatement preparedStatement = connection.prepareStatement(sqlQuery);
+            //preparedStatement.setString(1, String.valueOf(Integer.valueOf(roll_num)));
+            preparedStatement.setString(1, first_name);
+            preparedStatement.setString(2, name_initials);
+            preparedStatement.setString(3, address);
+            preparedStatement.setString(4, con_number);
+            preparedStatement.setString(5, gender);
+            preparedStatement.setString(6, email);
+
+
+            try{
+                preparedStatement.execute();
+                JOptionPane.showMessageDialog(null, "Your details successfully saved in to the system");
+
+            }catch (Exception exception){
+                JOptionPane.showMessageDialog(null, "Oops..!!!\n Something went wrong with your submission..!!!" + exception.getMessage());
+
+            }
+            connection.close();
+        }catch (Exception exception){
+            JOptionPane.showMessageDialog(null, "Oops..!!!\n Something went wrong with your submission..!!!" + exception.getMessage());
+
+        }
+    }
+
     private JButton BACKButton;
     private JTextField textField1;
     private JTextField textField2;
@@ -51,6 +86,38 @@ public class studentDetails {
             public void actionPerformed(ActionEvent e) {
                 studentDetailsFrame.dispose();
                 new studentHome();
+            }
+        });
+        SAVEButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                String first_name, name_initials, address, con_number, gender, email;
+                int roll_num;
+                JFrame msgFrame = new JFrame();
+
+                roll_num = Integer.parseInt(textField1.getText());
+                first_name = textField2.getText();
+                name_initials = textField3.getText();
+                address = textField4.getText();
+                con_number = textField5.getText();
+                gender = comboBox1.getSelectedItem().toString();
+                email = textField6.getText();
+
+                try{
+                    studentRegistration(roll_num, first_name, name_initials, address, con_number, gender, email);
+                } catch (Exception exception) {
+                    exception.printStackTrace();
+                }
+
+                textField1.setText("");
+                textField2.setText("");
+                textField3.setText("");
+                textField4.setText("");
+                textField5.setText("");
+                comboBox1.setSelectedItem("");
+                textField6.setText("");
+
             }
         });
     }
