26d7bd502585c725279c702ecae4a2a75b79a052
DILAKSHI-ICT <96301@fhss.sjp.ac.lk>
2021-07-13 15:47:00 +0530

    Created mysql class for getting connections of created database

---
+++ src/com/text/index/mysqlClass.java
@@ -0,0 +1,18 @@
+package com.text.index;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class mysqlClass {
+    static Connection connection;
+
+    public static Connection getConnection() throws Exception{
+
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        connection = DriverManager.getConnection("jdbc:mysql://localhost/qems", "root","");
+        System.out.println("Database Connected Successfully");
+        return connection;
+    }
+
+}
