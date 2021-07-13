561c0682e64a1814f8b4f0d6ad85ca601f782f89
DILAKSHI-ICT <96301@fhss.sjp.ac.lk>
2021-07-13 22:51:31 +0530

    Connected mysql code to add all student results in to the system

--- src/com/text/index/allStudentResult.java
+++ src/com/text/index/allStudentResult.java
@@ -1,11 +1,47 @@
 package com.text.index;
 
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 public class allStudentResult {
+
+    public void showAllResults(){
+        try {
+            Connection connection = mysqlClass.getConnection();
+            String sqlQuery = "SELECT * FROM `student_result`";
+
+            PreparedStatement preparedStatement = connection.prepareStatement(sqlQuery);
+
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            table1.setModel(new DefaultTableModel(
+                    null,
+                    new String[] {"Roll Number", "Student Name", "Result"}
+            ));
+            while (resultSet.next()){
+                String roll_num = resultSet.getString(1);
+                String stu_name = resultSet.getString(2);
+                String stu_result = resultSet.getString(3);
+
+                String data[] = {roll_num, stu_name, stu_result};
+                DefaultTableModel tableModel = (DefaultTableModel) table1.getModel();
+                tableModel.addRow(data);
+            }
+            resultSet.close();
+            preparedStatement.close();
+
+        }catch (Exception exception){
+            JOptionPane.showMessageDialog(null,"Oops !!!\nSomething went Wrong !!!\n"+exception.getMessage());
+        }
+    }
+
     private JTable table1;
     private JButton EXITButton;
     private JPanel allStudentResultPanel;
@@ -22,6 +58,8 @@ public class allStudentResult {
         allStudentResultFrame.pack();
         allStudentResultFrame.setLocationRelativeTo(null);
         allStudentResultFrame.setVisible(true);
+
+        showAllResults();
 
 
         EXITButton.addActionListener(new ActionListener() {
