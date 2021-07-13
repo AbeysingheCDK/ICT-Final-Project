bf862bf3626000955f3c646623f4a9687ad6182e
DILAKSHI-ICT <96301@fhss.sjp.ac.lk>
2021-07-13 16:27:08 +0530

    Created mysql code to add new question in to the system

--- src/com/text/index/addNewQuestion.java
+++ src/com/text/index/addNewQuestion.java
@@ -4,8 +4,44 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 
 public class addNewQuestion {
+
+    public void studentRegistration(int questionID, String question, String option1, String option2, String option3, String option4, String answer ){
+
+        try{
+            Connection connection = mysqlClass.getConnection();
+            String sqlQuery = "INSERT INTO `all_questions` (`questionID`, `question`, `option1`, `option2`, `option3`, `option4`, `answer`) VALUES (NULL, ?, ?, ?, ?, ?, ?)";
+
+            PreparedStatement preparedStatement = connection.prepareStatement(sqlQuery);
+            //preparedStatement.setString(1, String.valueOf(Integer.valueOf(roll_num)));
+            preparedStatement.setString(1, question);
+            preparedStatement.setString(2, option1);
+            preparedStatement.setString(3, option2);
+            preparedStatement.setString(4, option3);
+            preparedStatement.setString(5, option4);
+            preparedStatement.setString(6, answer);
+
+
+            try{
+                preparedStatement.execute();
+                JOptionPane.showMessageDialog(null, "New questions successfully saved in to the system");
+
+            }catch (Exception exception){
+                JOptionPane.showMessageDialog(null, "Oops..!!!\n Something went wrong with your submission..!!!" + exception.getMessage());
+
+            }
+            connection.close();
+        }catch (Exception exception){
+            JOptionPane.showMessageDialog(null, "Oops..!!!\n Something went wrong with your submission..!!!" + exception.getMessage());
+
+        }
+    }
+
     private JLabel addNewQuestion;
     private JButton exitButton;
     private JButton SAVEButton;
@@ -40,6 +76,40 @@ public class addNewQuestion {
                 if (a == 0)
                 addNewQuestionFrame.dispose();
                 new adminHome();
+
+            }
+        });
+        SAVEButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                String question, opt1, opt2, opt3, opt4, answer;
+                int questionID;
+                JFrame msgFrame = new JFrame();
+
+                questionID = Integer.parseInt(a00TextField.getText());
+                question = textField2.getText();
+                opt1 = textField3.getText();
+                opt2 = textField4.getText();
+                opt3 = textField5.getText();
+                opt4 = textField6.getText();
+                answer = textField7.getText();
+
+                try{
+                    studentRegistration(questionID, question, opt1, opt2, opt3, opt4, answer);
+                } catch (Exception exception) {
+                    exception.printStackTrace();
+                }
+
+                a00TextField.setText("");
+                textField2.setText("");
+                textField3.setText("");
+                textField4.setText("");
+                textField5.setText("");
+                textField6.setText("");
+                textField7.setText("");
+
+
 
             }
         });
