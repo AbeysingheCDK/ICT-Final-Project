1f676c173ba96a33f9f1a991939b701d95bbb083
DILAKSHI-ICT <96301@fhss.sjp.ac.lk>
2021-07-15 01:20:38 +0530

    Add table in to the quizePage GUI and connected with all_results database

--- src/com/text/index/quizPage.java
+++ src/com/text/index/quizPage.java
@@ -1,13 +1,53 @@
 package com.text.index;
 
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 
 public class quizPage {
+
+    public void showAllQuestions(){
+        try {
+            Connection connection = mysqlClass.getConnection();
+            String sqlQuery = "SELECT * FROM `all_questions`";
+
+            PreparedStatement preparedStatement = connection.prepareStatement(sqlQuery);
+
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            table1.setModel(new DefaultTableModel(
+                    null,
+                    new String[] {"questionID", "question", "option1", "option2", "option3", "option4"}
+            ));
+            while (resultSet.next()){
+                String queID = resultSet.getString(1);
+                String ques = resultSet.getString(2);
+                String opt1 = resultSet.getString(3);
+                String opt2 = resultSet.getString(4);
+                String opt3 = resultSet.getString(5);
+                String opt4 = resultSet.getString(6);
+
+
+                String data[] = {queID, ques, opt1, opt2, opt3, opt4};
+                DefaultTableModel tableModel = (DefaultTableModel) table1.getModel();
+                tableModel.addRow(data);
+            }
+            resultSet.close();
+            preparedStatement.close();
+
+        }catch (Exception exception){
+            JOptionPane.showMessageDialog(null,"Oops !!!\nSomething went Wrong !!!\n"+exception.getMessage());
+        }
+    }
+
+
     private JTextField textField1;
     private JTextField textField2;
     private JTextField textField3;
@@ -39,6 +79,8 @@ public class quizPage {
         quizPageFrame.setLocationRelativeTo(null);
         quizPageFrame.setVisible(true);
 
+        showAllQuestions();
+
 
         EXITButton.addActionListener(new ActionListener() {
             @Override
@@ -50,6 +92,12 @@ public class quizPage {
                     quizPageFrame.dispose();
                     new studentHome();
                 }
+
+            }
+        });
+        SUBMITButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
 
             }
         });
