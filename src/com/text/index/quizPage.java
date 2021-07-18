b28b4ee6a3ce3ba4c14472fa1b30dab792a7483a
DILAKSHI-ICT <96301@fhss.sjp.ac.lk>
2021-07-19 00:14:22 +0530

    Create methods to calculate students marks of the quiz exam.

--- src/com/text/index/quizPage.java
+++ src/com/text/index/quizPage.java
@@ -8,11 +8,20 @@ import java.awt.event.ActionListener;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.Statement;
 
 
 public class quizPage {
 
+    String stu_answers[] = new String[10];
+    int correct_answers = 0;
+    String result[] = new String[10];
+    String quizAnswers[] = new String[10];
+    String reg_no;
+
+
     public void showAllQuestions(){
+
         try {
             Connection connection = mysqlClass.getConnection();
             String sqlQuery = "SELECT * FROM `all_questions`";
@@ -26,7 +35,9 @@ public class quizPage {
                     null,
                     new String[] {"questionID", "question", "option1", "option2", "option3", "option4"}
             ));
+
             while (resultSet.next()){
+
                 String queID = resultSet.getString(1);
                 String ques = resultSet.getString(2);
                 String opt1 = resultSet.getString(3);
@@ -39,6 +50,7 @@ public class quizPage {
                 DefaultTableModel tableModel = (DefaultTableModel) table1.getModel();
                 tableModel.addRow(data);
             }
+
             resultSet.close();
             preparedStatement.close();
 
@@ -47,6 +59,90 @@ public class quizPage {
         }
     }
 
+    public void getAnswers(){
+
+        try {
+
+            Connection connection = mysqlClass.getConnection();
+            String sqlQuery = "SELECT answer FROM all_questions";
+
+            Statement stmt = connection.createStatement();
+            ResultSet resultSet = stmt.executeQuery(sqlQuery);
+            //System.out.println(resultSet);
+
+            int i = 0;
+
+            while (resultSet.next()) {
+                String  stu_result = resultSet.getString("answer");
+
+                //System.out.println(stu_result);
+
+            quizAnswers[i] = stu_result;
+            i += 1;
+
+            }
+
+            resultSet.close();
+
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
+    }
+
+    public void checkAnswer(String id, String stuAnswer) {
+
+        int convert_id =  Integer. parseInt(id)-1 ;
+
+        stu_answers[convert_id] =stuAnswer;
+
+    }
+
+    public void verifyAnswers(String reg_no) throws Exception {
+
+        for (int i = 0; i<10; i ++){
+            if (stu_answers[i].equals(quizAnswers[i])){
+                result[i] = "Correct";
+                correct_answers += 1;
+            }
+
+            else{
+                result[i] = "wrong";
+            }
+
+            //System.out.println("Student Answer "+stu_answers[i]);
+            //System.out.println("Quiz Answer " + quizAnswers[i]);
+        }
+
+        System.out.println("Total number of correct answers : " + correct_answers);
+    }
+    
+    public void quizRegistration(String reg_no, String stu_name){
+
+        try{
+
+            Connection connection = mysqlClass.getConnection();
+            String sqlQuery = "INSERT INTO `all_result` (`reg_no`, `stu_name`, `marks`) VALUES (?, ?, ?)";
+
+            PreparedStatement preparedStatement = connection.prepareStatement(sqlQuery);
+            preparedStatement.setString(1, reg_no);
+            preparedStatement.setString(2, stu_name);
+            preparedStatement.setString(3, String.valueOf(correct_answers));
+
+
+            try{
+                preparedStatement.execute();
+                JOptionPane.showMessageDialog(null, "Your details successfully added to the system");
+
+            }catch (Exception exception){
+                JOptionPane.showMessageDialog(null, "Oops..!!!\n Something went wrong with your submission..!!!" + exception.getMessage());
+            }
+
+            connection.close();
+
+        }catch (Exception exception){
+            JOptionPane.showMessageDialog(null, "Oops..!!!\n Something went wrong with your submission..!!!" + exception.getMessage());
+        }
+    }
 
     private JTextField textField1;
     private JTextField textField2;
@@ -62,11 +158,13 @@ public class quizPage {
     private JButton SUBMITButton;
     private JButton EXITButton;
     private JPanel quizPagePanel;
+    private JTextField textField11;
+    private JTextField textField12;
+    private JLabel marks;
     private JFrame quizPageFrame;
 
 
     public quizPage() {
-
 
         quizPageFrame = new JFrame("Quiz Exam Management System");
         quizPageFrame.setDefaultCloseOperation(WindowConstants.DO_NOTHING_ON_CLOSE);
@@ -92,12 +190,69 @@ public class quizPage {
                     quizPageFrame.dispose();
                     new studentHome();
                 }
-
             }
         });
+
+
         SUBMITButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+
+                String q1, q2, q3, q4, q5, q6, q7, q8, q9, q10;
+                JFrame msgFrame = new JFrame();
+
+                q1 = textField1.getText();
+                q2 = textField2.getText();
+                q3 = textField3.getText();
+                q4 = textField4.getText();
+                q5 = textField5.getText();
+                q6 = textField6.getText();
+                q7 = textField7.getText();
+                q8 = textField8.getText();
+                q9 = textField9.getText();
+                q10 = textField10.getText();
+
+                String[] data = {q1, q2, q3, q4, q5, q6, q7, q8, q9, q10};
+
+                int arrayIndex = 0;
+                int questionNo = 1;
+                 
+                getAnswers();
+
+                reg_no = textField11.getText();
+                String name = textField12.getText();
+
+                while(arrayIndex < 10)
+
+                    try{
+                        checkAnswer(String.valueOf(questionNo), data[arrayIndex]);
+                        arrayIndex += 1;
+                        questionNo += 1;
+
+                    } catch (Exception exception) {
+                        exception.printStackTrace();
+                    }
+
+                try {
+                    verifyAnswers(reg_no);
+                    quizRegistration(reg_no, name);
+
+                } catch (Exception exception) {
+                    exception.printStackTrace();
+                }
+
+                textField1.setText("");
+                textField2.setText("");
+                textField3.setText("");
+                textField4.setText("");
+                textField5.setText("");
+                textField6.setText("");
+                textField7.setText("");
+                textField8.setText("");
+                textField9.setText("");
+                textField10.setText("");
+
+                marks.setText(String.valueOf(correct_answers));
 
             }
         });
